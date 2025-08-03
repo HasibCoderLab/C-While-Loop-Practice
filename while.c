7a1d486da54fb16bb931e488c7067ced099c5e86
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-03 12:18:51 +0600

    update

--- while.c
+++ while.c
@@ -1,94 +1,94 @@
-// #include <stdio.h>
-// int main(){
-//     int i = 1;
-//     while(i <=5){
-//         printf("%d\n",i);
-//         i++;
-//     }
-// }
+#include <stdio.h>
+int main(){
+    int i = 1;
+    while(i <=5){
+        printf("%d\n",i);
+        i++;
+    }
+}
 
-// #include <stdio.h>
-// int main(){
-//  int n;
-//  printf("Enter Number : ");
-//  scanf("%d",&n);
-//  int i = 0;
-//  while(i<= n){
-//     printf("%d\n", i);
-//     i++;
-//  }
-//  return 0 ;
-// }
+#include <stdio.h>
+int main(){
+ int n;
+ printf("Enter Number : ");
+ scanf("%d",&n);
+ int i = 0;
+ while(i<= n){
+    printf("%d\n", i);
+    i++;
+ }
+ return 0 ;
+}
 
-// # include <stdio.h>
-//  int main(){
-//     int i = 0;
-//     while ( i <= 5){
-//         printf("%d\n",i);
-//         i++;
-//     }
-// return 0 ;
-//  }
+# include <stdio.h>
+ int main(){
+    int i = 0;
+    while ( i <= 5){
+        printf("%d\n",i);
+        i++;
+    }
+return 0 ;
+ }
 
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main (){
+int main (){
 
-// int i = 20;
-// while (i >= 20){
-// printf("%d\n",i);
-// i--;
-// }
-//     return 0 ;
-// }
+int i = 20;
+while (i >= 20){
+printf("%d\n",i);
+i--;
+}
+    return 0 ;
+}
 
 //   -------------------------------   do While  loop -----------
 
-// #include <stdio.h>
-// int main(){
-//     int i = 1;
-//   do{
-//     printf("%d\n", i);
-//     i++;
-//   }while(i<=5);
-//     return 0;
-// }
+#include <stdio.h>
+int main(){
+    int i = 1;
+  do{
+    printf("%d\n", i);
+    i++;
+  }while(i<=5);
+    return 0;
+}
 
-// #include <stdio.h>
-// int main(){
-//     int i = 5;
-//     do{
-//         printf("%d\n",i);
-//         i--;
-//     }while(i>=1);
-//     return 0 ;
-// }
+#include <stdio.h>
+int main(){
+    int i = 5;
+    do{
+        printf("%d\n",i);
+        i--;
+    }while(i>=1);
+    return 0 ;
+}
 
-// #include <stdio.h>
-// int main()
-// {
-//     int i = 10;
-//     do
-//     {
-//         printf("%d\n", i);
-//         i--;
-//     } while (i >= 1);
-//     return 0;
-// }
+#include <stdio.h>
+int main()
+{
+    int i = 10;
+    do
+    {
+        printf("%d\n", i);
+        i--;
+    } while (i >= 1);
+    return 0;
+}
 
 
 //    
 
 //  ------------   Review Learn Do  while loop ---------------------
 
-// #include <stdio.h>
-// int main(){
-//     int i = 1;
-//     do{
-//         printf("%d\n",i);
-//         i++;
-//     }while(i<=5); 
-//     return 0;
+#include <stdio.h>
+int main(){
+    int i = 1;
+    do{
+        printf("%d\n",i);
+        i++;
+    }while(i<=5); 
+    return 0;
 
-// }
+}
